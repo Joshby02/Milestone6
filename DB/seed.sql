c2ff6d5e613eb5716279d82e0470fd4684ab4970
DarthJarJar3 <calebscaten@gmail.com>
2026-02-18 12:40:02 -0700

    DataBase files

---
+++ DB/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email) VALUES
+  ('Rex Raptor', 'rex@dinocamp.com'),
+  ('Terry Dactyl', 'terry@dinocamp.com'),
+  ('Stacy Saurus', 'stacy@dinocamp.com'),
+  ('Bronty Brachios', 'bronty@dinocamp.com');
